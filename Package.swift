3076f9096fc8b6287c348577f5a7d4c2bb88848b
Phillip Cooper <phillip@myfiziq.com>
2022-12-05 15:13:31 +0800

    update for SPM dep

--- Package.swift
+++ Package.swift
@@ -10,12 +10,13 @@ let package = Package(
     ],
     products: [
         .library(
-            name: "OpenCV",
+            name: "AHIOpenCV",
             targets: ["opencv2"]),
     ],
     targets: [
         .binaryTarget(name: "opencv2",
                       url: "https://github.com/ahi-dev/OpenCV-SPM/releases/download/4.6.0/opencv2.xcframework.zip",
                       checksum: "1ead050146646a8f81e37aba0f7efb7033678311734a272f76557647c4d419b2"),
-    ]
+    ],
+    cxxLanguageStandard: .cxx11
 )
